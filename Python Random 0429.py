7e47ede819cd528db62a97ca6e59b69f93a1fd81
Zzzn <166351980+Zinin04@users.noreply.github.com>
2024-04-29 14:39:13 +0800

    Add files via upload

---
+++ Python Random 0429.py
@@ -0,0 +1,23 @@
+#import random
+#count= 1
+#while count <= 12:
+    #for i in range(1,6):
+       # randNum= random.randint(1,49)
+        #print('%3d'%(randNum),end=' ')
+   # print( )
+   # count +=1
+#print('Over')
+
+
+import random
+again = 1
+while again == 1:
+    for i in range(1,7):
+        randNum = random.randint(1,49)
+        print('%3d'%(randNum),end=' ')
+    print( )
+    again= 0 
+print('Over')
+
+
+
