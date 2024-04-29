d1f4286eb6c653c39dbefb497ccb85ed61959290
Zzzn <166351980+Zinin04@users.noreply.github.com>
2024-04-29 11:44:04 +0800

    Add files via upload

--- Python Random.py
+++ Python Random.py
@@ -1,18 +1,39 @@
-import random
-for i in range (1,11):
-    ranNum= random.randint(1,100)
-    print('%4d'%(ranNum),end=' ')
+#import random
+#for i in range (1,11):
+    #ranNum= random.randint(1,100)
+    #print('%4d'%(ranNum),end=' ')
     
 
-import random
-for i in range(1,11):
-    randNum=random.randint(1,100)
-    print('%4d'%(randNum),end=' ')
-    if randNum % 2 ==0:
-         even += 1
-    else:
-         odd += 1
-print('\neven=%d,odd=%d'%(even,odd))
+#import random
+#for i in range(1,11):
+    #randNum=random.randint(1,100)
+    #print('%4d'%(randNum),end=' ')
+    #if randNum % 2 ==0:
+         #even += 1
+   # else:
+         #odd += 1
+#print('\neven=%d,odd=%d'%(even,odd))
    
 
-
+import random
+times3 = 0
+times5 = 0
+times7 = 0
+others = 0
+for i in range (1,101):
+    flag=False
+    randNum=random.randint(1,100)
+    print(randNum,end=' ')
+    if randNum % 3 == 0:
+        times3 += 1
+        flag = True
+    if randNum%5==0:
+        times5 += 1
+        flag=True
+    if randNum%7==0:
+        times7+=1
+        flag=True
+    if flag == False:
+        others += 1
+print('\ntimes3 = %d, times5 =%d,times7=%d'%(times3, times5, times7))
+print('others=%d'%(others))
